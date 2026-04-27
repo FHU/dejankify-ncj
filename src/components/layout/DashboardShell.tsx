5bc4e6645ad858ca1e97b18161f9efc8ed84bcef
therealnima <nima.afsari@students.fhu.edu>
2026-04-26 20:49:38 -0500

    UseEffect issue

--- src/components/layout/DashboardShell.tsx
+++ src/components/layout/DashboardShell.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { useRouter, usePathname } from "next/navigation";
 import Sidebar from "@/components/layout/Sidebar";
 import Header from "@/components/layout/Header";
